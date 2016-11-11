696ff24e8507427508050791ec612bb9c08e56f8
Andrew Grexa <andrew@grexa.me>
2016-11-10 18:40:00 -0700

    Initial java palindrome

---
+++ java/src/palindrome/Palindrome.java
@@ -0,0 +1,32 @@
+package palindrome;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		System.out.println(Palindrome.isPalindrome("racecar"));
+		System.out.println(Palindrome.isPalindrome("raceca"));
+		System.out.println(Palindrome.isPalindrome(null));
+		System.out.println(Palindrome.isPalindrome(1234));
+	}
+	
+	public static boolean isPalindrome(String s) {
+		if (s == null) {
+			return false;
+		}
+		
+		char[] input = s.toCharArray();
+		int len = input.length;
+		
+		if (len == 0) {
+			return false;
+		}
+		
+		for (int i = 0; i < len / 2; i++) {
+			if (input[i] != input[len - i - 1]) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
---
+++ javascript/sum/sum.test.js
@@ -0,0 +1,27 @@
+import { comboSumsToX } from './sum';
+
+describe('comboSumsToX', () => {
+    var tests = [
+        [[1, 2, 3], 1, true],
+        [[3, 2, 1], 1, true],
+        [[1, 2, 3], 6, true],
+        [[3, 2, 1], 6, true],
+        [[1, 2, 3], 7, false],
+        [[3, 2, 1], 7, false],
+        [null, 0, false],
+        [undefined, 0, false],
+        [NaN, 0, false],
+        [{}, 0, false],
+        [1234, 0, false],
+        [true, 0, false],
+        [false, 0, false]
+    ];
+
+    tests.forEach((test) => {
+        let [input, x, output] = test;
+
+        it(`should return ${output} when called with ${JSON.stringify(input)}, ${x}`, () => {
+            expect(comboSumsToX(input, x)).toBe(output);
+        });
+    });
+});
